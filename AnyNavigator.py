a6b22d655a36b7d3b20089085525f79208b14103
Kazuyuki Kohno <kkohno@twitter.com>
2013-01-23 19:56:22 +0900

    initial commit

---
+++ AnyNavigator.py
@@ -0,0 +1,26 @@
+import sublime, sublime_plugin
+import re
+
+class AnyNavigateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        def on_done(result):
+            self.navigate(result)
+
+        self.view.window().show_input_panel('pattern', '', on_done, None, None)
+
+    def navigate(self, pattern):
+        regions = self.view.find_all(pattern)
+        regions = map(lambda _: self.view.line(_), regions)
+        items   = map(lambda _: self.view.substr(_), regions)
+
+        def on_done(index):
+            if index >= 0:
+                self.view.sel().clear()
+                region = regions[index]
+                e = self.view.begin_edit()
+                self.view.sel().clear()
+                self.view.sel().add(region)
+                self.view.show(region)
+                self.view.end_edit(e)
+
+        self.view.window().show_quick_panel(items, on_done)
---
+++ README.md
@@ -0,0 +1,12 @@
+AnyNavigator
+========
+
+Sublime Text 2 plugin to navigate you in current file.
+
+![Screenshot 1](http://i.imgur.com/rVXFnEd.png)
+
+![Screenshot 2](http://i.imgur.com/7EWWl8R.png)
+
+## Usage
+
+The `any_navigate` command is binded to `super+k + super+a`.
